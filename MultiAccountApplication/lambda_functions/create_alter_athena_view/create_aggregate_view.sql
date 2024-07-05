e7128cbda58ec84d59bf877acf3d03b97b610750
Katja Philipp <ktj@amazon.com>
2024-07-05 15:00:01 +0200

    Updated architecture diagram and added servermedianinefficiency to aggregate Athena view

--- MultiAccountApplication/lambda_functions/create_alter_athena_view/create_aggregate_view.sql
+++ MultiAccountApplication/lambda_functions/create_alter_athena_view/create_aggregate_view.sql
@@ -5,9 +5,10 @@ SELECT
 , entries.startdate
 , SUM(entries.mbmcarbon) mbmcarbon
 , inefficiency.gridmixinefficiency gridmixinefficiency
+, inefficiency.servermedianinefficiency servermedianinefficiency
 FROM
   (("${database_name}"."carbon_emissions_table" cet
 CROSS JOIN UNNEST(cet.emissions.carbonemissionentries) t (entries))
 CROSS JOIN UNNEST(cet.emissions.carbonemissionsinefficiency) t (inefficiency))
 WHERE (entries.startdate = inefficiency.startdate)
-GROUP BY cet.accountid, cet.query.querydate, entries.startdate, inefficiency.gridmixinefficiency
+GROUP BY cet.accountid, cet.query.querydate, entries.startdate, inefficiency.gridmixinefficiency, inefficiency.servermedianinefficiency
